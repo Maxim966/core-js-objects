bf0c86f8be09544ae39a454c02f5bdedf495703d
Maksim Sukhov <silveraven@yandex.ru>
2025-02-20 18:44:43 +0200

    feat: add solution makeWorld

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -132,8 +132,18 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arr1 = [];
+
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    const pairs = value.map((index) => [key, index]);
+    arr1.push(...pairs);
+  });
+
+  return arr1
+    .sort((a, b) => a[1] - b[1])
+    .map((item) => item[0])
+    .join('');
 }
 
 /**
